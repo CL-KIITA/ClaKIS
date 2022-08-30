25c16eef902d11a4b2fdba3eaec3a10d45119626
halka9000stg <csta19097@g.nihon-u.ac.jp>
2022-08-30 14:55:36 +0300

    add: clakis default resource

---
+++ lib/clakis_kernel/lib/default.resource.dart
@@ -0,0 +1,40 @@
+//package:clakis_kernel/default.resource.dart
+class CLaKISResource {
+final Map<String,Map<String,String>> fieldClass = {
+"C_": {"{iso1}en": "Classfication", "{iso1}ja": "言語コード・言語分類"},
+"CC": {"{iso1}en": "Code", "{iso1}ja": コード類(CLAコード、CSCコードなど)"},
+"CT": {"{iso1}en": "Taxonomy", "{iso1}ja": 分類(Se分類、モユネ分類など)"},
+"CX": {"{iso1}en": "Others", "{iso1}ja": その他"},
+"S_": {"{iso1}en": "Software System", "{iso1}ja": ソフトウェアシステム"},
+"SD": {"{iso1}en": "Dictionary", "{iso1}ja": 辞書"},
+"SP": {"{iso1}en": "Pronunciation/Phonology/Phonetics", "{iso1}ja": 発音・音韻・音声"},
+"SW": {"{iso1}en": "Word Creation", "{iso1}ja": 造語・語彙自動生成"},
+"SF": {"{iso1}en": "Inflection", "{iso1}ja": 語形変化"},
+"ST": {"{iso1}en": "Translator", "{iso1}ja": 翻訳機"},
+"SR": {"{iso1}en": "Character/Sound Recognition", "{iso1}ja": 文字認識・音声認識"},
+"SC": {"{iso1}en": "Corpus Management", "{iso1}ja": コーパス・例文管理"},
+"SS": {"{iso1}en": "Social Platform", "{iso1}ja": 交流サイト・SNS"},
+"SX": {"{iso1}en": "Others", "{iso1}ja": その他製作支援システム"},
+"E_": {"{iso1}en": "Encoding", "{iso1}ja": 文字エンコーディング"},
+"EE": {"{iso1}en": "Extended Encoding", "{iso1}ja": 文字エンコーディング拡張"},
+"EC": {"{iso1}en": "Custom Encoding", "{iso1}ja": 独自の文字エンコーディング"},
+"T_": {"{iso1}en": "Typesetting", "{iso1}ja": 組版・フォント"},
+"TR": {"{iso1}en": "Custom Font Renderer", "{iso1}ja": 独自フォントレンダラ"},
+"TF": {"{iso1}en": "Font Creation", "{iso1}ja": 個別フォント製作"},
+"TX": {"{iso1}en": "Others", "{iso1}ja": その他"},
+"D_": {"{iso1}en": "Documentation", "{iso1}ja": 文書化"},
+"DS": {"{iso1}en": "Sentence", "{iso1}ja": 人工言語の文例"},
+"DR": {"{iso1}en": "Reference", "{iso1}ja": 人工言語の解説書"},
+"DX": {"{iso1}en": "Others", "{iso1}ja": その他人工言語の資料"},
+"F_": {"{iso1}en": "Formal Description", "{iso1}ja": 形式的記述"},
+"FF": {"{iso1}en": "Formal Grammar", "{iso1}ja": 形式文法"},
+"FX": {"{iso1}en": "Others", "{iso1}ja": その他形式的記述"},
+"R_": {"{iso1}en": "Organisation Affairs", "{iso1}ja": 組織事務"},
+"RD": {"{iso1}en": "Organisation Identifier", "{iso1}ja": 組織識別表現(名前やシンボルマーク、ロゴ、ドメインなど)"},
+"RR": {"{iso1}en": "Resource", "{iso1}ja": 議事録・イベント資料"},
+"RB": {"{iso1}en": "Bots", "{iso1}ja": ボット"}
+"RX": {"{iso1}en": "Others", "{iso1}ja": その他"},
+};
+CLaKISResource();
+}
+final resCLaKIS = CLaKISResource();
